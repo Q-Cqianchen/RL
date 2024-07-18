6c195ef3db3d839fc6fdb9dbfd930c76e1845e7f
Q-Cqianchen <1723984553@qq.com>
2024-07-18 19:37:53 +0800

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dqn import DQNTrainer
+from ddqn import DDQNTrainer
 
 if __name__ == '__main__':
-    trainer = DQNTrainer(500,200, 5)
+    # trainer = DQNTrainer(500,200, 5)
+    trainer = DDQNTrainer(500, 200, 5)
     trainer.train()
